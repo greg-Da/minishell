2e3530b80937096894a3def23f09eed0cb4e2488
qbaret <qbaret@student.42nice.fr>
2025-06-18 14:37:40 +0200

    fix cd

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: qbaret <qbaret@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 13:07:45 by greg              #+#    #+#             */
-/*   Updated: 2025/06/18 14:23:05 by qbaret           ###   ########.fr       */
+/*   Updated: 2025/06/18 14:37:09 by qbaret           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,26 +69,7 @@ int	get_home(t_minish *manager)
 	return (0);
 }
 
-void	get_logical_parent(char *pwd, t_minish *manager)
-{
-	char	*copy;
-	char	*last_slash;
 
-	copy = ft_strdup(pwd);
-	last_slash = NULL;
-	if (!copy)
-		return ;
-	while (chdir(copy) != 0)
-	{
-		last_slash = ft_strrchr(copy, '/');
-		if (last_slash && last_slash != copy)
-			*last_slash = '\0';
-		else if (last_slash == copy)
-			copy[1] = '\0';
-	}
-	update_pwd(copy, pwd, manager);
-	free(copy);
-}
 
 static int	handle_go_home(t_minish *manager, char *pwd)
 {
@@ -103,47 +84,6 @@ static int	handle_go_home(t_minish *manager, char *pwd)
 	return (0);
 }
 
-int	handle_default(char **path, char *pwd, t_minish *manager)
-{
-	char	*new_pwd;
-	char	*tmp;
-	int		status;
-
-	new_pwd = getcwd(NULL, 0);
-	if (!new_pwd)
-	{
-		if (!ft_strcmp(path[1], "..") && pwd[ft_strlen(pwd - 1)] == '.'
-			&& pwd[ft_strlen(pwd - 2)] == '.')
-			get_logical_parent(pwd, manager);
-		else if (!ft_strcmp(path[1], ".."))
-		{
-			tmp = ft_strjoin(pwd, "/..");
-			update_pwd(tmp, pwd, manager);
-			free(tmp);
-			perror("cd: error retrieving current directory");
-			status = 1;
-		}
-		else
-			perror("getcwd");
-		status = 1;
-	}
-	update_pwd(new_pwd, pwd, manager);
-	free(new_pwd);
-	return (status);
-}
-
-int	handle_special(char **path, t_minish *manager, char *pwd)
-{
-	if (path[1] == NULL || !ft_strcmp(path[1], "~"))
-		return (handle_go_home(manager, pwd));
-	else if (!ft_strcmp(path[1], "-"))
-	{
-		handle_go_back(pwd, manager);
-		free(pwd);
-		return (0);
-	}
-	return (-1);
-}
 
 int	ft_cd(char **path, t_minish *manager)
 {
